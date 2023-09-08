08a6f9652e09b115262748a3627ec0260925d417
Nasser-Bash <nasseribrahim966@gmail.com>
2023-09-08 05:31:23 +0300

    update

--- components/layouts/header.js
+++ components/layouts/header.js
@@ -25,8 +25,8 @@ import { AnimatePresence, motion} from "framer-motion";
 
 
   return (
-    <nav className=" container mx-auto border-gray-200 dark:bg-primary sticky right-0 top-0 left-0 z-[100] ">
-    <div className="  max-w-screen -xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="  border-gray-200 dark:bg-primary sticky right-0 top-0 left-0 z-[100] ">
+    <div className=" container mx-auto max-w-screen -xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" className="flex items-center">
 
           <span className="self-center text-2xl text-second font-semibold whitespace-nowrap ">E.S</span>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '@/styles/globals.css'
 import '@/styles/App.css'
+import Head from 'next/head';
 import Layout from "../components/layouts/layout";
 export default function App({ Component, pageProps }) {
   return( 
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
+
+    <>
+    <Head>
+    <title>E.S</title>
+    </Head>
+        <Layout>
+            <Component {...pageProps} />
+         </Layout>
+    </>
+   
   )
 }
